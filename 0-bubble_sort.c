4478442a0c1082733cd54105c16efcdce1a34e47
khaled <empeshy20036969@gmail.com>
2024-03-01 16:54:56 +0200

    1st

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, n;
 	char swapped = 0;
 
-	if (!array || !size)
+	if (!array || !size || size < 2)
 		return;
 	n = size;
 	for (i = 0; i < size; i++)
