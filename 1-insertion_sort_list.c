5c2a963d670a210084cc923fa36f8508d6fa7cbb
khaled <empeshy20036969@gmail.com>
2024-03-03 18:11:37 +0200

    done

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insertion_sort_list - sort list via insertion sort
+ *
+ * @list: pointer to the head of the list
+ *
+ * Return: NOthing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *tmp, *new, *p;
+
+	if (!list || !(*list)->next)
+		return;
+
+	ptr = (*list)->next;
+	while (ptr)
+	{
+		tmp = ptr->prev;
+		p = ptr;
+		while (tmp && tmp->n > p->n)
+		{
+			if (p->next)
+				p->next->prev = tmp;
+			tmp->next = p->next;
+
+			if (!tmp->prev)
+				*list = p;
+			else
+				tmp->prev->next = p;
+
+			p->prev = tmp->prev;
+			tmp->prev = p;
+			p->next = tmp;
+
+			print_list(*list);
+			tmp = p->prev;
+		}
+
+		ptr = ptr->next;
+	}
+}
